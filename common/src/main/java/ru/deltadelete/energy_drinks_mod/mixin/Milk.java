8a8bde812d4f94bcb43bea65fcd5030c419bb6b0
deltaDelete <samansy@yandex.ru>
2023-06-27 19:52:25 +0700

    fix: forge version crash
    Differences in Forge and Fabric mappings in method name removeAllEffects of LivingEntity class

--- common/src/main/java/ru/deltadelete/energy_drinks_mod/mixin/Milk.java
+++ common/src/main/java/ru/deltadelete/energy_drinks_mod/mixin/Milk.java
@@ -2,49 +2,35 @@ package ru.deltadelete.energy_drinks_mod.mixin;
 
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MilkBucketItem;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.SoftOverride;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import ru.deltadelete.energy_drinks_mod.effects.EnergyDrinkEffect;
 import ru.deltadelete.energy_drinks_mod.registry.EffectsRegistry;
 
 
 @Mixin(MilkBucketItem.class)
-public class Milk {
-//    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z"),
-//            method = "finishUsingItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;")
-//    public void finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
-//        if (livingEntity.getActiveEffects().stream().anyMatch(mobEffectInstance ->
-//                mobEffectInstance.getEffect() instanceof EnergyDrinkEffect
-//        )) {
-//            livingEntity.removeAllEffects();
-//            livingEntity.addEffect(new MobEffectInstance(EffectsRegistry.DIARRHEA.get(), 24000));
-//        } else {
-//            livingEntity.removeAllEffects();
-//        }
-//    }
+public abstract class Milk {
+    public boolean hasEffect = false;
 
-    private boolean hasEffect = false;
-
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z"),
+    @Inject(at = @At("HEAD"),
             method = "finishUsingItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;")
-    public void PreEffectsRemove(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
-        hasEffect = (livingEntity.getActiveEffects().stream().anyMatch(mobEffectInstance ->
-                mobEffectInstance.getEffect() instanceof EnergyDrinkEffect
-        ));
+    public void PreEffectsRemove(ItemStack stack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
+        if (!level.isClientSide) {
+            hasEffect = livingEntity.hasEffect(EffectsRegistry.ENERGY_DRINK.get());
+        }
     }
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;removeAllEffects()Z", shift = At.Shift.AFTER),
+    @Inject(at = @At("RETURN"),
             method = "finishUsingItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;")
-    public void PostEffectsRemove(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
-        if (hasEffect)
+    public void PostEffectsRemove(ItemStack stack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir) {
+        if (!level.isClientSide && hasEffect) {
             livingEntity.addEffect(new MobEffectInstance(EffectsRegistry.DIARRHEA.get(), 24000));
+        }
     }
 }
